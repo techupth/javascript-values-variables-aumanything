b08763a4cc7bf31096a9b274928a901f2a604ec0
thiranattada <thiranattada.n@muze.co.th>
2024-05-23 13:16:42 +0700

    feat: javascript exercise 1

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,23 @@
+// ให้ Declare ตัว Variable ที่สามารถ Reassign Value ได้ ทั้งหมด 3 Variable ตามข้อกำหนดดังต่อไปนี้
+
+// 1.1 Variable ที่เก็บ Value ที่เป็นชื่อของคนซึ่งมีชื่อคือ "Karter"
+// 1.2. Variable ที่เก็บ Value ที่เป็นนามสกุลของคนซึ่งมีนามสกุลคือ "Anderson"
+// 1.3. Variable ที่เก็บ Value ที่เป็นที่อยู่ซึ่งมีที่อยู่เป็น "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
+// ให้ Declare ตัว Variable ที่ไม่สามารถ Reassign Value ได้ ทั้งหมด 3 Variable ตามข้อกำหนดดังต่อไปนี้
+
+// 1.4 Variable ที่เก็บ Value ชื่อร้านอาหารซึ่งมีชื่อเป็น "Chef's Table by Chef James"
+// 1.5 Variable ที่เก็บ Value ของราคาอาหารสุทธิในบิลนั้นซึ่งมีราคาเป็น 15000
+// 1.6 Variable ที่เก็บ Value ของจำนวนร้านอาหารที่เปิดอยู่ตอนนี้ทั้งหมดซึ่งมี 1500 ร้าน
+
+
 //Exercise #1: Variable Creation
 //Start coding here
+let firstName = "Karter";
+let lastName = "Anderson";
+let postAddress = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+const RESTAURANT_NAME = "Chef's Table by Chef James";
+const NET_PRICE = 15000;
+const NUM_OPENED_RESTAURANT = 1500; 
 
 //Exercise #2: Variable Reassignment
 //Start coding here
